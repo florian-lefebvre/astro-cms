d337e748c1424853d86c0baedaa837913a9ffaf9
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-04-13 19:24:21 +0200

    feat: work on logic

--- package/src/integration.ts
+++ package/src/integration.ts
@@ -1,32 +1,53 @@
 import { fileURLToPath } from "node:url";
-import type { AstroIntegration } from "astro"
+import { addVitePlugin, createResolver, defineIntegration } from "astro-integration-kit";
+import { z } from "astro/zod";
+import { resolveConfig, type ResolvedConfig } from "./config.js";
 
-export const integration = (): AstroIntegration => {
-	return {
-		name: "astro-cms",
-		hooks: {
-			"astro:config:setup": ({ updateConfig, config }) => {
-				let cmsConfig: any;
+export const integration = defineIntegration({
+	optionsSchema: z
+		.object({
+			configFile: z.string().default("./cms.config.ts"),
+			base: z.string().default("/cms")
+		})
+		.default({}),
+	name: "astro-cms",
+	setup({ options }) {
+		const { resolve } = createResolver(import.meta.url)
 
-				updateConfig({
-					vite: {
-						plugins: [
-							{
-								name: "astro-cms-vite-plugin",
-								async configureServer(viteServer) {
-									const configPath = fileURLToPath(
-										new URL("./cms.config.ts", config.root),
-									);
+		let resolvedConfig: ResolvedConfig
 
-									const mod = await viteServer.ssrLoadModule(configPath);
-									cmsConfig = mod.default;
-									console.log(cmsConfig);
-								},
+		return {
+			hooks: {
+				"astro:config:setup": (params) => {
+					const { config, injectRoute, logger } = params
+
+					if (config.output === "static") {
+						logger.error("`output: 'static'` is not supported.")
+						throw new Error("astro-cms failed.")
+					}
+
+					addVitePlugin(params, {
+						plugin: {
+							name: "astro-cms-vite-plugin",
+							async configureServer(viteServer) {
+								const configPath = fileURLToPath(
+									new URL(options.configFile, config.root),
+								);
+
+								const mod = await viteServer.ssrLoadModule(configPath);
+								resolvedConfig = resolveConfig(mod.default)
+								console.dir(resolvedConfig, { depth: null });
 							},
-						],
-					},
-				});
-			}
-		}
-	}
-}
+						},
+					});
+
+					injectRoute({
+						entrypoint: resolve("../assets/routes/index.astro"),
+						pattern: `${options.base}/`,
+						prerender: false
+					})
+				},
+			},
+		};
+	},
+});
--- playground/astro.config.mts
+++ playground/astro.config.mts
@@ -1,17 +1,19 @@
-import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import { createResolver } from "astro-integration-kit";
 import { hmrIntegration } from "astro-integration-kit/dev";
-
-const {default: cms} = await import("astro-cms")
+import node from "@astrojs/node";
+const { default: cms } = await import("astro-cms");
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
-		tailwind(),
 		cms(),
 		hmrIntegration({
 			directory: createResolver(import.meta.url).resolve("../package/dist"),
 		}),
 	],
+	output: "hybrid",
+	adapter: node({
+		mode: "standalone",
+	}),
 });
